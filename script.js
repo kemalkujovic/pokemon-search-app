d0751ed760db44f54359cedfffbb53cd0a270e46
kemalkujovic <kemalkujovic1111@gmail.com>
2022-11-25 16:41:59 +0100

    display

--- script.js
+++ script.js
@@ -5,27 +5,39 @@ let errorDiv = document.querySelector(".error-div");
 let mainPokemon = document.querySelector(".main");
 let logo = document.querySelector("#logo");
 let pokemonName = document.querySelector(".pokemonName");
+searchBtn.addEventListener("click", fetchFunction);
 
-searchBtn.addEventListener("click", async function () {
+async function fetchFunction() {
   try {
     let response = await axios.get(BASE_URL + searchInput.value.toLowerCase());
     displayPokemon(response.data);
   } catch (error) {
-    errorMessage();
+    if (error.response.status === 404) {
+      errorMessage();
+    }
+    console.log(error);
   }
-});
+}
 function errorMessage() {
   errorDiv.innerHTML = "";
+  mainPokemon.style.display = "none";
   errorDiv.style.display = "flex";
   let errorM = document.createElement("p");
   errorM.innerText = "We don't know that pokemon!";
   errorDiv.insertAdjacentElement("beforeend", errorM);
 }
 function displayPokemon(data) {
+  errorDiv.style.display = "none";
   let name = data.forms[0].name;
+  let typeName = data.types[0].type.name;
   mainPokemon.style.display = "flex";
   logo.src = data.sprites.other.dream_world.front_default;
   pokemonName.innerText = name.toUpperCase()[0] + name.slice(1, name.length);
   document.querySelector(".hpText").innerHTML = `HP ${data.stats[0].base_stat}`;
   document.querySelector(".xpText").innerHTML = `XP ${data.base_experience}`;
+  document.querySelector(".weight").innerHTML = `${data.weight}kg`;
+  document.querySelector(".height").innerHTML = `${data.height}m`;
+  document.querySelector(".fire").innerHTML = `${
+    typeName.toUpperCase()[0] + typeName.slice(1, typeName.length)
+  }`;
 }
